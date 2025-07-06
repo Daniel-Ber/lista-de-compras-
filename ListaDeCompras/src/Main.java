b8b4f6e5ac19b94032a95a98d252d763d37916f8
Daniel-Ber <danieldkdr9@gmail.com>
2025-07-06 17:49:41 -0300

    What was done :  - Handle exceptions (e.g., InputMismatchException when typing a string instead of a number)
    To-Do :
     - Test the code and fix any new issues
        - Add an option to show items marked as bought
        - Add a way to return to the previous menu after selecting a numeric option (e.g., go back after choosing option 1)

--- ListaDeCompras/src/Main.java
+++ ListaDeCompras/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 //USE GIT STATUS AND GIT LOG BEFORE WORK ON IT
 public class Main {
@@ -12,11 +13,12 @@ public class Main {
     public static void main(String[] args)  {
      Scanner sc = new Scanner(System.in);
      boolean stopWhile = false;
+     Boolean yesOrNo = null;
+     int optionForSwitch = 0;
 
         print("Hello user , would you like to create a list? [yes/no]");
         String read = sc.nextLine();
-        Boolean yesOrNo = null;
-        int optionForSwitch;
+
 
         if (read.equals("yes")){
             yesOrNo = true;
@@ -47,33 +49,43 @@ public class Main {
                 //List was created
 
                 //Using the list
-                //Here have to be recursive
                 //We have to add a new function: See the list[x], made like a to-do (Bought itens).
                 while (!stopWhile){
-                    print("Press 1 to add a new item, 2 to delete an item (item was bought),3 to see the list or 4 to close the program: ");
-                    optionForSwitch = sc.nextByte();
-                    sc.nextLine();
-                    switch (optionForSwitch) {
-                        case 1 :
-                            print("Type the item : "  );
-                            String newItem = sc.nextLine();
-                            shoppingList.addElementShoppingList(newItem);
-                            printLn("The item " + newItem + " was aditioned on the list");
-                            break;
-                        case 2 :
-                            print("Type the item that you want to delet: ");
-                            String delItem = sc.nextLine();
-                            shoppingList.deletElementShoppingList(delItem);
-                            printLn("The item" + delItem + "was deleted on the list");
-                            break;
-                        case 3 :
-                            shoppingList.showElementsList();
-                            break;
-                        case 4 :
-                            print("Ok, good to see you =)");
-                            stopWhile = true;
-                            break;
+                    try{
+                        print("Press 1 to add a new item, 2 to " +
+                                "delete an item (item was bought),3 to see the list" +
+                                " or 4 to " +
+                                "close the program: ");
+                        String input = sc.nextLine();
+                        optionForSwitch = Integer.parseInt(input);
+
+
+                        switch (optionForSwitch) {
+                            case 1 :
+                                print("Type the item : "  );
+                                String newItem = sc.nextLine();
+                                shoppingList.addElementShoppingList(newItem);
+                                printLn("The item " + newItem + " was aditioned on the list");
+                                break;
+                            case 2 :
+                                print("Type the item that you want to delet: ");
+                                String delItem = sc.nextLine();
+                                shoppingList.deletElementShoppingList(delItem);
+                                printLn("The item" + delItem + "was deleted on the list");
+                                break;
+                            case 3 :
+                                shoppingList.showElementsList();
+                                break;
+                            case 4 :
+                                print("Ok, good to see you =)");
+                                stopWhile = true;
+                                break;
+                        }
+                    }catch (NumberFormatException exception){
+                        printLn("Please provide a valid text message : ");
                     }
+
+
                 }
 
 
